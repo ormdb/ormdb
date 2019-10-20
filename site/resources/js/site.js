948d288980ab90c80a1c4b4efc30e31e4ff7ad4e
ormdb <ormdb@outlook.com>
2019-10-20 13:52:02 +0300

    .

---
+++ site/resources/js/site.js
@@ -0,0 +1,11 @@
+﻿site = (function () {
+    var location_path = location.href.split("/");
+    location_path.pop();
+    location_path = location_path.join("/");
+
+    return {
+        code_path: location_path + "/site/docs/codes/",
+        wiki_path: location_path + "/site/docs/wiki/",
+        copyright: 'Copyright(c) ' + (new Date()).getFullYear() + ' - OrmDb'
+    };
+})();
